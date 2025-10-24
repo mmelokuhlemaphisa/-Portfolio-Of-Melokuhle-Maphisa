451e92acf013cc6066f31c94675d37d72a66e80f
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-10-24 08:47:13 +0200

    feat:remove import react

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/footer";
 
